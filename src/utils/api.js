d9f0ee2c653f01bc3cf77314a8bba4d3a25dc938
Rajesh <rajeshsrrs2@gmail.com>
2025-09-13 13:42:01 +0530

    Added route

--- src/utils/api.js
+++ src/utils/api.js
@@ -6,7 +6,7 @@ const API_URL = import.meta.env.VITE_API_URL
   : '/api'
 
 const api = axios.create({
-  baseURL: API_URL,
+  baseURL: `${API_URL}/api`,
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json'
